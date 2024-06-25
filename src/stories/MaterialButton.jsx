ccc9bf662a63864b0b68ebd3d176bbbcd14544db
Molly Lin <mollylin2014@gmail.com>
2024-06-25 17:31:21 +0800

    add 按鈕禁用狀態時的 css-in-js 寫法

--- src/stories/MaterialButton.jsx
+++ src/stories/MaterialButton.jsx
@@ -1,5 +1,17 @@
 import Button from '@mui/material/Button';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+
+
+const disabledStyle = styled(Button)`
+  &:disabled {
+    pointer-events: visible;
+    cursor: not-allowed;
+  }
+`;
 
 /**
  * `Button` 元件代表一個可點擊的按鈕，在使用者點擊之後會觸發相對應的業務邏輯。
@@ -8,14 +20,23 @@ export const muiButton = ({
   variant,
   label,
   color,
-  isDisabled
+  isDisabled,
+  ...props
 }) => {
+  const isDisabledStyle = isDisabled ? disabledStyle : null;
   return (
     <Button
       type="button"
       variant={variant}
       color={color}
       disabled={isDisabled}
+      css={css(`
+        &:disabled {
+          pointer-events: visible;
+          cursor: not-allowed;
+        }
+        `)}
+      {...props}
     >
       {label}
     </Button>
