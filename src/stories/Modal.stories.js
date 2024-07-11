3a0ced0d0cfd7889b7004dfbeffd2f5983693069
Molly Lin <mollylin2014@gmail.com>
2024-07-11 11:35:35 +0800

    add MUI's Modal

---
+++ src/stories/Modal.stories.js
@@ -0,0 +1,12 @@
+import { MuiModal } from './Modal';
+
+export default {
+  title: 'Material/Modal',
+  component: MuiModal,
+};
+
+export const Basic = {
+  args: {
+    variant: "contained"
+  }
+}
