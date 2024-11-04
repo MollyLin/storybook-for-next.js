b6098b82fe0ea46ee82af264dfef229c92e09a2c
Molly Lin <mollylin2014@gmail.com>
2024-11-04 11:00:44 +0800

    Refactor ESLint configuration and update linting scripts
    根據這篇文章升級成功: https://blog.linotte.dev/eslint-9-next-js-935c2b6d0371
    沒有使用 Next.js provides's integrated ESLint: https://nextjs.org/docs/app/building-your-application/configuring/eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,22 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import js from '@eslint/js';
+import {FlatCompat} from '@eslint/eslintrc';
+// source: https://eslint.org/blog/2024/05/eslint-compatibility-utilities/#fixing-an-imported-configuration
+import {fixupConfigRules} from '@eslint/compat';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
-export default [...compat.extends("next/core-web-vitals", "plugin:storybook/recommended")];
+
+const patchedConfig = fixupConfigRules([
+  ...compat.extends('next/core-web-vitals', 'plugin:storybook/recommended'),
+]);
+
+const config = [...patchedConfig, {ignores: ['.next/*']}];
+
+export default config;
